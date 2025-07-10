26679cbdb133a145595ac4e2c050dd7ed33bf142
KrilinLH <133998950+KrilinLH@users.noreply.github.com>
2025-07-10 15:13:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,32 +9,52 @@ terraform {
 
 provider "azurerm" {
   features {}
-}
-
-data "azurerm_client_config" "main" {
+  subscription_id = "10ce0944-5960-42ed-8657-1a8177030014"
 }
 
 data "azurerm_container_registry" "main" {
   name                = "ci-asr243-student1"
-  resource_group_name = "rg-asr243-student1"
+  resource_group_name = "rg-asr243-mfo"
 }
 
-resource "azurerm_resource_group" "main" {
+data "azurerm_resource_group" "main" {
   name     = "rg-asr243-student1"
 }
 
+resource "azurerm_user_assigned_identity" "acrpull" {
+  location            = data.azurerm_resource_group.main.location
+  name                = "uid-acrpull"
+  resource_group_name = data.azurerm_resource_group.main.name
+}
+
+resource "azurerm_role_assignment" "ci_acrpull" {
+  scope                = data.azurerm_container_registry.main.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.acrpull.principal_id
+}
+
 resource "azurerm_container_group" "nginx" {
-  name                = "example-continst"
+  name                = "ci-asr243-mfo"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
   ip_address_type     = "Public"
-  dns_name_label      = "ci-asr243-student1_2"
+  dns_name_label      = "ci-asr243-mfo"
   os_type             = "Linux"
+  
+  image_registry_credential {
+    user_assigned_identity_id = azurerm_user_assigned_identity.acrpull.id
+    server                    = data.azurerm_container_registry.main.login_server
+  }
+
+  identity {
+    type = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.acrpull.id]
+  }
 
   container {
-    name   = "NGINX"
+    name   = "nginx"
     image  = "acrasr243mfo.azurecr.io/nginx:latest"
-    cpu    = "0.5"
+    cpu    = "1"
     memory = "1.5"
 
     ports {
@@ -42,14 +62,6 @@ resource "azurerm_container_group" "nginx" {
       protocol = "TCP"
     }
   }
-}
 
-identity {
-type = "SystemAssigned"
-}
-
-resource "azurerm_role_assignment" "ci_acrpull" {
-  scope                = data.azurerm_container_registry.main.id
-  role_definition_name = "Reader"
-  principal_id         = azurerm_container_group.nginx.identity[0].principal_id
+  depends_on = [ azurerm_role_assignment.ci_acrpull ]
 }
